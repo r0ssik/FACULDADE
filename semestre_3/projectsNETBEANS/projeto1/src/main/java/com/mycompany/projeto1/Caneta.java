abf4add302d9d037f15d945e5cfb31bc8865588f
r0ssik <93134791+r0ssik@users.noreply.github.com>
2025-03-06 19:21:07 -0300

    mudando de pasta

--- semestre_3/projectsNETBEANS/projeto1/src/main/java/com/mycompany/projeto1/Caneta.java
+++ semestre_3/projectsNETBEANS/projeto1/src/main/java/com/mycompany/projeto1/Caneta.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.projeto1;
+package semestre_3.projectsNETBEANS.projeto1.src.main.java.com.mycompany.projeto1;
 
 /**
  *
--- semestre_3/projectsNETBEANS/projeto1/src/main/java/com/mycompany/projeto1/Projeto1.java
+++ semestre_3/projectsNETBEANS/projeto1/src/main/java/com/mycompany/projeto1/Projeto1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.projeto1;
+package semestre_3.projectsNETBEANS.projeto1.src.main.java.com.mycompany.projeto1;
 
 /**
  *
