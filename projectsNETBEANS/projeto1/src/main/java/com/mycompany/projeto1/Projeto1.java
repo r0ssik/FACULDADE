ddf20077b6b337b878b3251676e777e2a40432c9
r0ssik <93134791+r0ssik@users.noreply.github.com>
2025-03-05 21:36:41 -0300

    começando POO

---
+++ projectsNETBEANS/projeto1/src/main/java/com/mycompany/projeto1/Projeto1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto1;
+
+/**
+ *
+ * @author rossik
+ */
+public class Projeto1 {
+
+    public static void main(String[] args){
+        System.out.println("Começando a brincar com POO");
+        
+         Caneta caneta1 = new Caneta();
+         caneta1.cor = "preto";
+         caneta1.tampada = true;
+         caneta1.carga = 100;
+         
+         caneta1.status();
+    }
+}
