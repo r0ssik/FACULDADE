faba8e1e8aaa0ea701403541a02730cc82f8a0a4
r0ssik <93134791+r0ssik@users.noreply.github.com>
2025-04-16 19:16:02 -0300

    teste final

--- semestre_3/EDD/semana10/20250410/20250410/TesteListaLigada.java
+++ semestre_3/EDD/semana10/20250410/20250410/TesteListaLigada.java
@@ -13,12 +13,20 @@ public class TesteListaLigada {
     b.idade = 30;
 
     lista.inserir(b);
+
+    Pessoa c = new Pessoa();
+    c.nome = "João";
+    c.idade = 30;
+
+    lista.inserir(c);
     lista.imprimir();
 
     System.out.println(lista.imprimirQuantidade());
-    //lista.removerInicio();
-
+    lista.removerInicio();
+    lista.imprimir();
+    System.out.println(lista.imprimirQuantidade());
     lista.removerFinal();
+    System.out.println(lista.imprimirQuantidade());
     lista.imprimir();
   }
 }
